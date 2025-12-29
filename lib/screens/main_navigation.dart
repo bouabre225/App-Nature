552080395b55f61b89ce0f5ed444ecd40acfde03
Florent BOUDZOUMOU <florentboudz@gmail.com>
2025-12-29 18:37:36 +0100

    Feat: initialisation de mainNavigation

---
+++ lib/screens/main_navigation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class MainNavigation extends StatefulWidget{
+  const MainNavigation({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
